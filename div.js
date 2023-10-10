7baefeb775ea1b1cebd03c46f9cbec769bd761d4
SilviaVillafranca <svillafranca57@gmail.com>
2023-10-10 12:31:08 +0200

    noaveprev

--- div.js
+++ div.js
@@ -1,29 +1,40 @@
+// Definisci una variabile globale per tenere traccia dell'indice della pagina corrente
+var currentPageIndex = 0;
+
+// Funzione per aggiornare il numero di pagina e mostrare/nascondere i div delle pagine
+function updatePageCounter() {
+    var pageCounter = document.getElementById('page-counter');
+    var totalDivs = document.querySelectorAll('.video-container.active').length;
+    
+    // Aggiorna il testo del numero di pagina corrente
+    pageCounter.textContent = (currentPageIndex + 1) + '/' + totalDivs;
+    
+    // Mostra solo il div con l'indice corrispondente
+    var divs = document.querySelectorAll('.video-container');
+    for (var i = 0; i < divs.length; i++) {
+        if (i === currentPageIndex) {
+            divs[i].classList.add('active');
+        } else {
+            divs[i].classList.remove('active');
+        }
+    }
+}
+
 // Funzione per navigare all'elemento successivo
 function goToNext() {
-    // Trova l'elemento corrente
-    var currentDiv = document.querySelector('.video-container.active');
-
-    // Trova l'elemento successivo
-    var nextDiv = currentDiv.nextElementSibling;
-
-    // Se esiste un elemento successivo, passa ad esso
-    if (nextDiv) {
-        currentDiv.classList.remove('active');
-        nextDiv.classList.add('active');
+    if (currentPageIndex < document.querySelectorAll('.video-container.active').length - 1) {
+        currentPageIndex++;
+        updatePageCounter();
     }
 }
 
 // Funzione per tornare all'elemento precedente
 function goToPrevious() {
-    // Trova l'elemento corrente
-    var currentDiv = document.querySelector('.video-container.active');
-
-    // Trova l'elemento precedente
-    var previousDiv = currentDiv.previousElementSibling;
-
-    // Se esiste un elemento precedente, torna ad esso
-    if (previousDiv) {
-        currentDiv.classList.remove('active');
-        previousDiv.classList.add('active');
+    if (currentPageIndex > 0) {
+        currentPageIndex--;
+        updatePageCounter();
     }
 }
+
+// Chiamate iniziali per impostare il numero di pagina corrente
+updatePageCounter();
