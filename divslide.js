245a2573f54995647985bdc410dc00c30a544dab
SilviaVillafrank <140407700+SilviaVillafrank@users.noreply.github.com>
2023-12-22 13:23:59 +0100

    jsgotodiv

--- divslide.js
+++ divslide.js
@@ -1,20 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
   let currentPage = 1; // Pagina corrente
   const totalPages = 4; // Numero totale di pagine
 
-  function showPage(pageNumber) {
-    document.querySelectorAll('.video-container').forEach(page => {
-      page.classList.remove('active');
-    });
-
-    document.getElementById(`div${pageNumber}`).classList.add('active');
-
-    currentPage = pageNumber;
-    updatePreview();
-  }
-
   function goToDiv(page) {
-    showPage(page); // Rimuove i primi tre caratteri ("div") dall'id
+    showPage(page); 
   }
   
 
@@ -30,20 +18,5 @@ function goToNext(page) {
   }
 }
 
-function updatePreview() {
-  const previewImages = document.querySelectorAll('.image-item');
-
-  previewImages.forEach((image, index) => {
-    image.classList.remove('active');
-    if (index + 1 === currentPage) {
-      image.classList.add('active');
-    }
-  });
-}
-
-// Inizializza l'anteprima
-updatePreview();
-
 // Mostra la prima pagina inizialmente
 showPage(currentPage);
-});
