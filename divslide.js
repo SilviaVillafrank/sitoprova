5b5ba879465914e2ce513c81fd3d6c12b354793c
SilviaVillafrank <140407700+SilviaVillafrank@users.noreply.github.com>
2023-12-22 14:26:38 +0100

    href

--- divslide.js
+++ divslide.js
@@ -1,32 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let currentPage = 1; // Pagina corrente
-  const totalPages = 5; // Numero totale di pagine
+let currentPage = 1; // Pagina corrente
+const totalPages = 5; // Numero totale di pagine
 
- const previewSlideshow = document.getElementById("preview-slideshow");
-
-  function addPreviewItem(src, onclickFunction) {
-    const previewItem = document.createElement("div");
-    previewItem.classList.add("preview-item");
-    previewItem.innerHTML = `<img src="${src}" onclick="${onclickFunction}">`;
-    previewSlideshow.appendChild(previewItem);
-  }
-
-  function showPage(pageNumber) {
-    // Nasconde tutte le pagine
-    document.querySelectorAll('.video-container').forEach(page => {
-      page.classList.remove('active');
-    });
-
-    // Mostra solo la pagina desiderata
-    document.getElementById(`div${pageNumber}`).classList.add('active');
-
-    // Aggiorna la pagina corrente
-    currentPage = pageNumber;
-
-    // Aggiorna l'anteprima
-    updatePreview();
-  }
-
+ 
   function goToDiv(page) {
     showPage(page);
   }
@@ -43,23 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
- function updatePreview() {
-    // Rimuovi tutte le miniature
-    while (previewSlideshow.firstChild) {
-      previewSlideshow.removeChild(previewSlideshow.firstChild);
-    }
-
-    // Aggiungi nuove miniature
-    for (let i = 1; i <= totalPages; i++) {
-      const src = `DEVMAN980/Diapositiva${i}.JPG`;
-      const onclickFunction = `goToDiv(${i})`;
-      addPreviewItem(src, onclickFunction);
-    }
-  }
-
-  // Inizializza l'anteprima
-  updatePreview();
-
   // Mostra la prima pagina inizialmente
   showPage(currentPage);
-});
+
